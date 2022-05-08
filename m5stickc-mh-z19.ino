0782f2733b7dbebc85849e5f95b0036320b3c1eb
Erik Smit <erik.lucas.smit@gmail.com>
2022-05-08 13:39:50 +0200

    use printf() to replace multiple print()s

--- m5stickc-mh-z19.ino
+++ m5stickc-mh-z19.ino
@@ -1,14 +1,14 @@
 #include <M5StickC.h>
 #include "MHZ.h"
 
-#define DEBUG 1
+#define DEBUG
 
 #ifdef DEBUG
 #define D_SerialBegin(...) Serial.begin(__VA_ARGS__)
-#define D_print(...)    Serial.print(__VA_ARGS__)
-#define D_write(...)    Serial.print(__VA_ARGS__)
-#define D_println(...)  Serial.println(__VA_ARGS__)
-#define D_printf(...)  Serial.printf(__VA_ARGS__)
+#define D_print(...)       Serial.print(__VA_ARGS__)
+#define D_write(...)       Serial.print(__VA_ARGS__)
+#define D_println(...)     Serial.println(__VA_ARGS__)
+#define D_printf(...)      Serial.printf(__VA_ARGS__)
 #else
 #define D_SerialBegin(...)
 #define D_print(...)
@@ -60,33 +60,22 @@ void setup() {
 }
 
 void loop() {
+  int ppm_uart = 0;
+  int temperature = 0;
+  
   M5.update(); // need to call update()
   M5.Lcd.setCursor(0, 2, 1);
     
-  D_print("\n----- Time from start: ");
-  D_print(millis() / 1000);
-  D_println(" s");
+  D_printf("----- Time from start: %ds\n", millis() / 1000);
 
-  int ppm_uart = co2.readCO2UART();
-  D_print("PPMuart: ");
-
-  if (ppm_uart > 0) {
-    D_print(ppm_uart);
-  } else {
-    D_print("n/a");
-  }
-
+  ppm_uart = co2.readCO2UART();
+#ifdef DEBUG
+  temperature = co2.getLastTemperature();
+#endif
+  
   M5.Lcd.printf("CO2: %.4d ppm\r\n", ppm_uart);
+  D_printf("PPMuart: %d, Temp: %d \n", ppm_uart, temperature);
+  D_println("------------------------------");
 
-  int temperature = co2.getLastTemperature();
-  D_print(", Temperature: ");
-
-  if (temperature > 0) {
-    D_println(temperature);
-  } else {
-    D_println("n/a");
-  }
-
-  D_println("\n------------------------------");
-  delay(5000);
+  delay(10000);
 }
